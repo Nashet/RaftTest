0ced3ce285ab6b516cd53b7c517ef82e256ad1c1
Nashet <Nashet@users.noreply.github.com>
2018-07-05 10:03:38 +0300

    Add name space

--- Assets/RaftTest/Scripts/Builder.cs
+++ Assets/RaftTest/Scripts/Builder.cs
@@ -2,44 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.Characters.FirstPerson;
-
-/// <summary>
-/// Allows FPS camera to build blocks
-/// </summary>
-public class Builder : MonoBehaviour, ICharacter
+namespace RaftTest
 {
     /// <summary>
-    /// Whatever players holds in hands
+    /// Allows FPS camera to build blocks
     /// </summary>
-    public IHoldable Holds { get; private set; }
-
-    [SerializeField] private GameObject debugCube;
-
-    public void TakeInHand(IHoldable placeable)//todo refactor with prefabs?
+    public class Builder : MonoBehaviour, ICharacter
     {
-        if (Holds != null) // hides previous object in hands
+        /// <summary>
+        /// Whatever players holds in hands
+        /// </summary>
+        public IHoldable Holds { get; private set; }
+
+        [SerializeField] private GameObject debugCube;
+
+        public void TakeInHand(IHoldable placeable)//todo refactor with prefabs?
         {
-            Holds.Hide();
+            if (Holds != null) // hides previous object in hands
+            {
+                Holds.Hide();
+            }
+            Holds = placeable;
+            if (Holds != null) // shows new object in hands
+            {
+                Holds.Show();
+            }
         }
-        Holds = placeable;
-        if (Holds != null) // shows new object in hands
+
+        // Update is called once per frame
+        void Update()
         {
-            Holds.Show();
+            if (Holds != null)
+                Holds.UpdateBlock();
+
+            // places small cube at looking position, for debugging
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                debugCube.transform.position = hit.point;
+            }
         }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Holds != null)
-            Holds.UpdateBlock();        
-
-        // places small cube at looking position, for debugging
-        RaycastHit hit;
-        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
-        {
-            debugCube.transform.position = hit.point;
-        }        
-    }    
 }
-
